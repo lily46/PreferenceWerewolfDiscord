05041c08d82b0e06b6214d6d2507a7798c576749
ys <lily@MacBook-Pro.local>
2022-08-02 00:41:18 +0900

    参加者一覧出したり

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def on_message(message):
     if message.content.startswith("/game /start"):
         registered_list = []
         selected = None
-        await message.channel.send("ゲームを開始するよ！")
+        await message.channel.send("ゲームを開始するもふ！")
 
     # start inference
     if message.content.startswith("/game /discuss"):
@@ -27,29 +27,55 @@ async def on_message(message):
         word = None
         for key in selected.keys():
             word = selected[key]
-        await message.channel.send("お題は\"{}\"だよ！みんなで順番にお題について語りながら，誰のお題か当ててみてね！".format(word))
+
+        str = ""
+        for pair in registered_list:
+            for key in pair.keys():
+                str = str + key + " "
+                #print(str)
+        await message.channel.send("お題は\"{}\"だもふ！みんなで順番にお題について語りながら，誰のお題か当ててもふ！参加者は{}もふ".format(word, str))
 
     # get state
     if message.content.startswith("/game /state"):
         num = len(registered_list)
-        await message.channel.send("現在{}人が言葉を登録しているよ！".format(num))
+
+        str = ""
+        for pair in registered_list:
+            for key in pair.keys():
+                str = str + key + " "
+                #print(str)
+
+        # str
+        await message.channel.send("現在{}人({})が言葉を登録しているもふ！".format(num, str))
+
+    if message.content.startswith("/game /vote"):
+        str = "!poll \"狼はだれ？\""
+        for pair in registered_list:
+           for key in pair.keys():
+                str = str + " \"" + key + "\""
+
+        await message.channel.send(str)
+
+    # get state
+    if message.content.startswith("/game /wine"):
+        await message.channel.send("<:wineshou:915268406134059039>")
 
     # show answer
     if message.content.startswith("/game /answer"):
         if selected is not None:
             await message.channel.send("答え：{}".format(selected))
         else:
-            await message.channel.send("手順が間違っていたみたい，やり直してね！")
+            await message.channel.send("手順が間違っていたみたい，やり直してもふ！(discussされないかももふ)")
 
     # Register Word
     if message.content.startswith("/game /register "):
         if client.user != message.author:
             split_word = message.content.split(" ")
             if len(split_word) < 3:
-                await message.channel.send("もう一度試してみてね！")
+                await message.channel.send("もう一度試してみてもふ！　半角スペース使ってるか確認してもふ")
             else:
                 added_word = split_word[2]
                 registered_list.append({message.author.name:added_word})
-                await message.channel.send("ワードを受け付けたよ！:{}:{}".format(message.author.name, added_word))
+                await message.channel.send("ワードを受け付けたもふ！:{}:{}".format(message.author.name, added_word))
 
 client.run("") # ""の間にトークンを記載
